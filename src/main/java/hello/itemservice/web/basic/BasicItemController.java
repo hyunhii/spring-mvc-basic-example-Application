6e6ff679b8c5dabcc02700dc620ea108df9338d5
hyunhii <be_hyunny@naver.com>
2023-11-08 01:41:53 +0900

    feat: 스프링 MVC 2편 학습을 위한 소스 코드 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -105,7 +105,7 @@ public class BasicItemController {
     /**
      * 테스트용 데이터 추가
      */
-    @PostConstruct
+//    @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
